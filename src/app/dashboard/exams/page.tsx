a140e60a00c0f0f1475c6b404315af23fc6cf41e
Rock Rock <rock64coc@gmail.com>
2025-04-20 20:09:20 +0000

    start exam should open exam window

--- src/app/dashboard/exams/page.tsx
+++ src/app/dashboard/exams/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
@@ -10,6 +10,8 @@ import { db } from '@/lib/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { useToast } from '@/hooks/use-toast';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Separator } from '@/components/ui/separator';
 
 interface Question {
     questionText: string;
@@ -35,6 +37,32 @@ export default function ExamInterface() {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [studentAnswers, setStudentAnswers] = useState<string[]>([]); // Array to store student answers
     const { toast } = useToast();
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(false);
+
+    useEffect(() => {
+        const getCameraPermission = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                setHasCameraPermission(true);
+
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                }
+            } catch (error) {
+                console.error('Error accessing camera:', error);
+                setHasCameraPermission(false);
+                toast({
+                    variant: 'destructive',
+                    title: 'Camera Access Denied',
+                    description: 'Please enable camera permissions in your browser settings to use this app.',
+                });
+            }
+        };
+
+        getCameraPermission();
+    }, []);
+
 
     useEffect(() => {
         const fetchExam = async () => {
@@ -43,6 +71,7 @@ export default function ExamInterface() {
                     title: 'Error: Exam ID not found.',
                     variant: 'destructive',
                 });
+                router.push("/dashboard");
                 return;
             }
 
@@ -56,6 +85,7 @@ export default function ExamInterface() {
                         title: 'Error: Exam not found.',
                         variant: 'destructive',
                     });
+                    router.push("/dashboard");
                 }
             } catch (error: any) {
                 console.error('Error fetching exam:', error);
@@ -64,11 +94,12 @@ export default function ExamInterface() {
                     description: error.message,
                     variant: 'destructive',
                 });
+                router.push("/dashboard");
             }
         };
 
         fetchExam();
-    }, [examId, toast]);
+    }, [examId, toast, router]);
 
     useEffect(() => {
         let interval: any;
@@ -133,45 +164,77 @@ export default function ExamInterface() {
     const currentQuestion = questions[currentQuestionIndex];
 
     return (
-        <div className="container mx-auto py-10">
-            <Card className="w-full">
-                <CardContent className="flex flex-col space-y-4">
-                    <div className="flex items-center justify-between">
-                        <h2 className="text-2xl font-bold">{exam.examName}</h2>
-                        <div className="flex items-center space-x-2 text-lg">
-                            <Clock className="h-5 w-5" />
-                            <span>{formatTime(timer)}</span>
+        <div className="flex h-screen bg-background">
+            <div className="w-2/3 p-4 space-y-4">
+                <Card className="h-full flex flex-col">
+                    <CardContent className="space-y-4">
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-2xl font-bold">{exam.examName}</h2>
+                            <div className="flex items-center space-x-2 text-lg">
+                                <Clock className="h-5 w-5" />
+                                <span>{formatTime(timer)}</span>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <h3 className="text-xl font-semibold">Question {currentQuestionIndex + 1}</h3>
-                        <p>{currentQuestion.questionText}</p>
-                        {currentQuestion.options && currentQuestion.options.length > 0 ? (
-                            <RadioGroup value={studentAnswers[currentQuestionIndex]} onValueChange={(value) => handleOptionSelect(value)}>
-                                {currentQuestion.options.map((option, index) => (
-                                    <div key={index} className="flex items-center space-x-2">
-                                        <RadioGroupItem value={option} id={`option-${index}`} className="h-5 w-5" />
-                                        <label htmlFor={`option-${index}`} className="cursor-pointer">{option}</label>
-                                    </div>
-                                ))}
-                            </RadioGroup>
-                        ) : (
-                            <p>No options available for this question.</p>
-                        )}
-                    </div>
-                    <div className="flex justify-between">
-                        <Button variant="secondary" onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
-                            Previous
-                        </Button>
-                        {currentQuestionIndex < questions.length - 1 ? (
-                            <Button onClick={handleNextQuestion}>Next</Button>
-                        ) : (
-                            <Button onClick={handleSubmitExam}>Submit Exam</Button>
-                        )}
-                    </div>
-                    <Button onClick={() => router.push("/dashboard")}>Back to Dashboard</Button>
-                </CardContent>
-            </Card>
+                        <div className="mb-4 p-4 rounded-md bg-secondary">
+                            <h3 className="text-xl font-semibold">Question {currentQuestionIndex + 1}</h3>
+                            <p>{currentQuestion.questionText}</p>
+                            <Separator className="my-2" />
+                            {currentQuestion.options && currentQuestion.options.length > 0 ? (
+                                <RadioGroup value={studentAnswers[currentQuestionIndex]} onValueChange={(value) => handleOptionSelect(value)}>
+                                    {currentQuestion.options.map((option, index) => (
+                                        <div key={index} className="flex items-center space-x-2">
+                                            <RadioGroupItem value={option} id={`option-${index}`} className="h-5 w-5" />
+                                            <label htmlFor={`option-${index}`} className="cursor-pointer">{option}</label>
+                                        </div>
+                                    ))}
+                                </RadioGroup>
+                            ) : (
+                                <p>No options available for this question.</p>
+                            )}
+                        </div>
+                        <div className="flex justify-between">
+                            <Button variant="secondary" onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
+                                Previous
+                            </Button>
+                            {currentQuestionIndex < questions.length - 1 ? (
+                                <Button onClick={handleNextQuestion}>Next</Button>
+                            ) : (
+                                <Button onClick={handleSubmitExam}>Submit Exam</Button>
+                            )}
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+            <div className="w-1/3 p-4 space-y-4">
+                <Card className="h-1/2">
+                    <CardContent>
+                        <h3 className="text-lg font-semibold mb-2">Video preview</h3>
+                        <video ref={videoRef} className="w-full aspect-video rounded-md" autoPlay muted />
+                        {!(hasCameraPermission) && (
+                            <Alert variant="destructive">
+                                <AlertTitle>Camera Access Required</AlertTitle>
+                                <AlertDescription>
+                                    Please allow camera access to use this feature.
+                                </AlertDescription>
+                            </Alert>
+                        )
+                        }
+                    </CardContent>
+                </Card>
+                <Card className="h-1/2">
+                    <CardContent>
+                        <h3 className="text-lg font-semibold mb-2">Questions and Status</h3>
+                        <p>Progress: {currentQuestionIndex + 1} / {questions.length}</p>
+                        <div className="overflow-y-auto h-32">
+                            {questions.map((question, index) => (
+                                <div key={index} className="py-1">
+                                    Question {index + 1}: {question.questionText.substring(0, 50)}...
+                                </div>
+                            ))}
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     );
 }
